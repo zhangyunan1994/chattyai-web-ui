b0dd7c6545b45b1e2172efb826075f57a11950ee
zhangyunan <zyndev@gmail.com>
2023-05-08 19:42:45 +0800

    后端页面

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(process.cwd(), 'src'),
